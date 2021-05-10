0b9dfafba402570d1ed76aa707bb0b5943746b60
schenckb <78156138+schenckb@users.noreply.github.com>
2021-05-10 08:22:47 -0400

    Update RSVP.js

--- RSVP.js
+++ RSVP.js
@@ -1,5 +1,32 @@
+
+
+<head>
+<script>
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+</script>
 <script>
 document.getElementById("button button1").addEventListener("click", function() {
   alert("Hello World!");
 });
 </script>
+</head>
+
+<body onload="startTime()">
+
+<div id="txt"></div>
+
+</body>
